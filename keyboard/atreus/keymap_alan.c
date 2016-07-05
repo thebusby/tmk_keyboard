696cebf50d7dd95f1f04cb8b8224e1870c9d5eec
Alan Busby <thebusby@thebusby.com>
2016-07-05 16:56:27 +0900

    fn0 doubles as backspace, and fn0 now doubles as enter

--- keyboard/atreus/keymap_alan.c
+++ keyboard/atreus/keymap_alan.c
@@ -10,8 +10,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {	\
   { /* FN layer 0, qwerty for OS based qwerty -> dvorak translation */
     {KC_TAB,  KC_Q, KC_W, KC_E, KC_R, KC_T,                                      KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC }, \
     {ACTION_MODS_TAP_KEY(MOD_LSFT, ACTION_MODS_ONESHOT(MOD_LSFT)), KC_A, KC_S, KC_D, KC_F, KC_G,        KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT}, \
-    {KC_LALT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N,                                KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_FN3}, \
-    {KC_LGUI, KC_DELETE, KC_ESC, KC_O, KC_BSPC, KC_FN0,                          KC_LCTL, KC_FN11, KC_KANA, KC_ESC, ACTION_MODS_TAP_KEY(MOD_RSFT, KC_ENT), KC_PGDN} \
+    {KC_LALT, KC_Z, KC_X, KC_C, KC_V, KC_B,                                      KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_FN3}, \
+    {KC_LGUI, KC_DELETE, KC_ESC, KC_O, KC_BSPC, KC_FN13,                          ACTION_MODS_TAP_KEY(MOD_LCTL, KC_ENT), KC_FN11, KC_KANA, KC_ESC, ACTION_MODS_TAP_KEY(MOD_RSFT, KC_ENT), KC_PGDN} \
   }, \
 
   { /* FN layer 1, numbers, arrows, and extended punctuation */
@@ -67,6 +67,7 @@ const uint16_t PROGMEM fn_actions[] = {
 
   [11]  = ACTION_LAYER_TAP_KEY(1, KC_SPC), // FN11, to Fn overlap or space
   [12]  = ACTION_LAYER_TAP_KEY(4, KC_SPC), // FN12, to Fn overlap or space
+  [12]  = ACTION_LAYER_TAP_KEY(1, KC_BSPC), // FN12, to Fn overlap or space
 };
 
 void action_function(keyrecord_t *record, uint8_t id, uint8_t opt)
