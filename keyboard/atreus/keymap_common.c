eb0911a9d44c9192de0dfb64782132e3931d9726
Alan Busby <thebusby@thebusby.com>
2016-07-07 17:14:21 +0900

    still figuring out some thumb positioning

--- keyboard/atreus/keymap_common.c
+++ keyboard/atreus/keymap_common.c
@@ -30,7 +30,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {	\
     {KC_TAB,  KC_Q, KC_W, KC_E, KC_R, KC_T,                                      KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC }, \
     {KC_FN15, KC_A, KC_S, KC_D, KC_F, KC_G,                                      KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT}, \
     {KC_LALT, KC_Z, KC_X, KC_C, KC_V, KC_B,                                      KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_0}, \
-    {KC_LGUI, KC_DELETE, KC_ESC, MY_RO, KC_FN13, KC_FN16,                        KC_FN14, KC_FN11, MY_KANA, KC_ESC, KC_ENT, KC_PGDN} \
+    {KC_LGUI, KC_DELETE, KC_ESC, MY_RO, KC_FN16, KC_FN13,                        KC_FN14, KC_FN11, MY_KANA, KC_ESC, KC_ENT, KC_PGDN} \
   }, \
 
   { /* FN layer 1, numbers, arrows, and extended punctuation */
